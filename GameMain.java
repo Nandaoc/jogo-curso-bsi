d2a002156119d36b2a6266a359eef985d87ad2ac
Nandaoc <nandaocarvalho11@gmail.com>
2025-01-27 21:22:43 -0300

    tarefa 19 - criando classe main

---
+++ GameMain.java
@@ -0,0 +1,13 @@
+/**
+ * Esta classe é a principal e dá início ao jogo.
+ * 
+ * @author  Fernanda Carvalho e Marcella Del Rio
+ * @version 27/01/2025 :D
+ */
+public class GameMain
+{
+    public void main(String name, int age) {
+        Game game = new Game();
+        game.play(name, age);
+    }
+}
